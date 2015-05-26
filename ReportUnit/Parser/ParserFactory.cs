726505d73c3ab4ab8ec42a08505c1f77472b431c
Sandra Greenhalgh <sandra.greenhalgh@ticketmaster.co.uk>
2015-05-26 13:11:05 +0100

    update ParserFactory checks against report runner file types

--- ReportUnit/Parser/ParserFactory.cs
+++ ReportUnit/Parser/ParserFactory.cs
@@ -27,9 +27,7 @@
 				return null;
 			}
 
-            IParser fileParser = null; 
-            
-            fileExtension = fileExtension.ToLower();
+            IParser fileParser = null;
 
             switch (TestRunnerType(resultsFile))
             {
@@ -66,34 +64,46 @@
                 if (doc.DocumentElement == null)
                     return TestRunner.Unknown;
 
-                // NUnit
-                XmlNode envNode = doc.SelectSingleNode("//environment");
 
-                if (envNode != null && envNode.Attributes["nunit-version"] != null)
-                    return TestRunner.NUnit;
+	            string fileExtension = Path.GetExtension(filePath).ToLower();
+				if (fileExtension.EndsWith("trx"))
+				{
+					// -------------------------------------------------------------------------------------------------------
+					// MSTest2010
+					nsmgr = new XmlNamespaceManager(doc.NameTable);
+					nsmgr.AddNamespace("ns", "http://microsoft.com/schemas/VisualStudio/TeamTest/2010");
 
-                // Gallio
-                nsmgr = new XmlNamespaceManager(doc.NameTable);
-                nsmgr.AddNamespace("ns", "http://www.gallio.org/");
+					// check if its a mstest 2010 xml file 
+					// will need to check the "//TestRun/@xmlns" attribute - value = http://microsoft.com/schemas/VisualStudio/TeamTest/2010
+					XmlNode testRunNode = doc.SelectSingleNode("ns:TestRun", nsmgr);
+					if (testRunNode != null && testRunNode.Attributes != null && testRunNode.Attributes["xmlns"] != null && testRunNode.Attributes["xmlns"].InnerText.Contains("2010"))
+					{
+						return TestRunner.MSTest2010;
+					}
+				}
 
-                XmlNode model = doc.SelectSingleNode("//ns:testModel", nsmgr);
+				if (fileExtension.EndsWith("xml"))
+				{
+					// -------------------------------------------------------------------------------------------------------
+		            // Gallio
+		            nsmgr = new XmlNamespaceManager(doc.NameTable);
+		            nsmgr.AddNamespace("ns", "http://www.gallio.org/");
 
-                if (model != null)
-                    return TestRunner.Gallio;
+		            XmlNode model = doc.SelectSingleNode("//ns:testModel", nsmgr);
+		            if (model != null) return TestRunner.Gallio;
 
-                // MSTest2010
-                if (Path.GetExtension(filePath).ToLower().Contains("trx"))
-                {
-                    nsmgr = new XmlNamespaceManager(doc.NameTable);
-                    nsmgr.AddNamespace("ns", "http://microsoft.com/schemas/VisualStudio/TeamTest/2010");
 
-                    // check is mstest 2010 xml file 
-                    // will need to check the "//TestRun/@xmlns" attribute - value = http://microsoft.com/schemas/VisualStudio/TeamTest/2010
-                    XmlNode testRunNode = doc.SelectSingleNode("ns:TestRun", nsmgr);
+					// -------------------------------------------------------------------------------------------------------
+					// NUnit
+					// NOTE: not all nunit test files (ie when have nunit output format from other test runners) will contain the environment node
+					//			but if it does exist - then it should have the nunit-version attribute
+					XmlNode envNode = doc.SelectSingleNode("//environment");
+					if (envNode != null && envNode.Attributes != null && envNode.Attributes["nunit-version"] != null) return TestRunner.NUnit;
 
-                    if (testRunNode != null && testRunNode.Attributes["xmlns"] != null)
-                        return TestRunner.MSTest2010;
-                }
+					// check for test-suite nodes - if it has those - its probably nunit tests
+					var testSuiteNodes = doc.SelectNodes("//test-suite");
+					if (testSuiteNodes != null && testSuiteNodes.Count > 0) return TestRunner.NUnit;
+				}
             }
             catch { }
 
