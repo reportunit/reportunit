fbf4a203c8f0ecd4d09da76d0c487ac632a4def5
Carlos Fernández <carlosrfa@outlook.com>
2016-03-22 20:28:28 +0100

    Added XSD from JUnits output to validate XML

---
+++ ReportUnit/Parser/JUnit.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using ReportUnit.Model;
+
+namespace ReportUnit.Parser
+{
+    internal class JUnit : IParser
+    {
+        public Report Parse(string filePath)
+        {
+            if (!System.IO.File.Exists(filePath))
+            {
+                throw new FileNotFoundException(string.Format("File {0} does not exist.", filePath));
+            }
+
+            XDocument doc = XDocument.Load(filePath);
+
+
+
+            return new Report();
+        }
+    }
+}
