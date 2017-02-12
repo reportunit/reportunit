22928d2b87ffc886085035fc8a480bded0e08caf
Artum <aptem.tchachkin@gmail.com>
2017-02-12 15:08:33 +0200

    Trying vNu for HTML validation

--- ReportUnitTest/JUnitTests.cs
+++ ReportUnitTest/JUnitTests.cs
@@ -17,21 +17,21 @@ namespace ReportUnitTest
         public static void Setup()
         {
             var assemblyDir = Path.GetDirectoryName(new Uri(Assembly.GetExecutingAssembly().CodeBase).LocalPath);
-            TestContext.Progress.Write("AssemblyDir: " + assemblyDir);
+            TestContext.Progress.WriteLine("AssemblyDir: " + assemblyDir);
             if (assemblyDir == null || !Directory.Exists(assemblyDir))
             {
                 throw new Exception("Failed to get assembly path");
             }
             
             ResourcesDir = Path.Combine(assemblyDir, "..", "..", "Resources");
-            TestContext.Progress.Write("ResourcesDir: " + ResourcesDir);
+            TestContext.Progress.WriteLine("ResourcesDir: " + ResourcesDir);
             if (!Directory.Exists(ResourcesDir))
             {
                 throw new Exception("Can't find Resources folder");
             }
 
             ExecutableDir = Path.Combine(assemblyDir, "..", "..", "..", "ReportUnit", "bin");
-            TestContext.Progress.Write("ExecutableDir: " + ExecutableDir);
+            TestContext.Progress.WriteLine("ExecutableDir: " + ExecutableDir);
             if (!Directory.Exists(ExecutableDir))
             {
                 throw new Exception("Can't find ReportUnit folder");
@@ -42,15 +42,80 @@ namespace ReportUnitTest
                 throw new Exception("Can't find ReportUnit.exe");
             }
         }
-
+        
         [Test]
-        public void Test()
+        public void test_junit_one_testsuite_multiple_testcases()
         {
-            var filename = Path.Combine(ExecutableDir, "ReportUnit.exe");
+            var processInfo = PrepareProcess("ReportUnit.exe", "test_junit_one_testsuite_multiple_testcases.xml");
+
+            RunProcess(processInfo, 5000);
+
+            ValidateHtmlReport("test_junit_one_testsuite_multiple_testcases.html");
+        }
+
+        private static void ValidateHtmlReport(string htmlReportFileName)
+        {
+            var htmlFile = Path.Combine(ResourcesDir, "JUnit", htmlReportFileName);
+            if (!File.Exists(htmlFile))
+            {
+                throw new Exception("No HTML report");
+            }
+
+            var vNuJarDirectory = Path.Combine(ResourcesDir, "vnu.jar_17.2.1");
+            ProcessStartInfo processInfo = new ProcessStartInfo()
+            {
+                FileName = "java",
+                Arguments = "-Xss512k -jar vnu.jar " + htmlFile,
+                RedirectStandardError = true,
+                RedirectStandardOutput = true,
+                RedirectStandardInput = false,
+                CreateNoWindow = true,
+                UseShellExecute = false,
+                WorkingDirectory = vNuJarDirectory
+            };
+
+            RunProcess(processInfo, 10000);
+        }
+
+        private static void RunProcess(ProcessStartInfo processInfo, int milliseconds)
+        {
+            TestContext.Progress.WriteLine("Start Process...");
+            TestContext.Progress.WriteLine("Filename: " + processInfo.FileName);
+            TestContext.Progress.WriteLine("Arguments: " + processInfo.Arguments);
+
+            var proc = Process.Start(processInfo);
+            if (proc == null)
+            {
+                throw new Exception("Failed to start");
+            }
+            if (!proc.WaitForExit(milliseconds))
+            {
+                throw new Exception("Timeout");
+            }
+
+            while (!proc.StandardOutput.EndOfStream)
+            {
+                TestContext.Progress.WriteLine(proc.StandardOutput.ReadLine());
+            }
+
+            while (!proc.StandardError.EndOfStream)
+            {
+                TestContext.Progress.WriteLine(proc.StandardError.ReadLine());
+            }
+
+            if (proc.ExitCode != 0)
+            {
+                throw new Exception("Exit code " + proc.ExitCode);
+            }
+        }
+
+        private ProcessStartInfo PrepareProcess(string reportUnitFileName, string junitXmlFileName)
+        {
+            var filename = Path.Combine(ExecutableDir, reportUnitFileName);
             ProcessStartInfo processInfo = new ProcessStartInfo()
             {
                 FileName = filename,
-                Arguments = Path.Combine(ResourcesDir, "JUnit", "test_junit_01.xml"),
+                Arguments = Path.Combine(ResourcesDir, "JUnit", junitXmlFileName),
                 RedirectStandardError = true,
                 RedirectStandardOutput = true,
                 RedirectStandardInput = false,
@@ -65,87 +130,13 @@ namespace ReportUnitTest
                 processInfo.Arguments = filename + " " + processInfo.Arguments;
             }
 
-            TestContext.Progress.Write("Start Process...");
-            TestContext.Progress.Write("Filename: " + processInfo.FileName);
-            TestContext.Progress.Write("Arguments: " + processInfo.Arguments);
-
-            var proc = Process.Start(processInfo);
-            if (proc == null)
-            {
-                throw new Exception("Failed to start");
-            }
-            if (!proc.WaitForExit(5000))
-            {
-                throw new Exception("Timeout");
-            }
-
-            while (!proc.StandardOutput.EndOfStream)
-            {
-                TestContext.Progress.Write(proc.StandardOutput.ReadLine());
-            }
-
-            while (!proc.StandardError.EndOfStream)
-            {
-                TestContext.Progress.Write(proc.StandardError.ReadLine());
-            }
-
-            if (proc.ExitCode != 0)
-            {
-                throw new Exception("Exit code " + proc.ExitCode);
-            }
-
-            var htmlFile = Path.Combine(ResourcesDir, "JUnit", "test_junit_01.html");
-            if (!File.Exists(htmlFile))
-            {
-                throw new Exception("No HTML report");
-            }
-            //W3CValidate(htmlFile);
+            return processInfo;
         }
 
-        private bool IsRunningOnMono()
+        private static bool IsRunningOnMono()
         {
             return Type.GetType("Mono.Runtime") != null;
         }
-
-
-        public static void W3CValidate(string htmlFile)
-        {
-            // Create a request using a URL that can receive a post.   
-            WebRequest request = WebRequest.Create("http://validator.nu/&out=gnu");
-            
-            // Set the Method property of the request to POST.  
-            request.Method = "POST";
-
-            byte[] byteArray = Encoding.UTF8.GetBytes(File.ReadAllText(htmlFile));
-
-            // Set the ContentType property of the WebRequest.  
-            request.ContentType = "text/html; charset=utf-8";
-
-            // Set the ContentLength property of the WebRequest.  
-            request.ContentLength = byteArray.Length;
-            // Get the request stream.  
-            Stream dataStream = request.GetRequestStream();
-            // Write the data to the request stream.  
-            dataStream.Write(byteArray, 0, byteArray.Length);
-            // Close the Stream object.  
-            dataStream.Close();
-            // Get the response.  
-            WebResponse response = request.GetResponse();
-            // Display the status.  
-            Console.WriteLine(((HttpWebResponse)response).StatusDescription);
-            // Get the stream containing content returned by the server.  
-            dataStream = response.GetResponseStream();
-            // Open the stream using a StreamReader for easy access.  
-            StreamReader reader = new StreamReader(dataStream);
-            // Read the content.  
-            string responseFromServer = reader.ReadToEnd();
-            // Display the content.  
-            Console.WriteLine(responseFromServer);
-            // Clean up the streams.  
-            reader.Close();
-            dataStream.Close();
-            response.Close();
-        }
-
+        
     }
 }
